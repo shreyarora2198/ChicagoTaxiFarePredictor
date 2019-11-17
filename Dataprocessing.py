fe87ce78eb87fc41f40b5581743fe16107dc1f3b
jfb5557 <jfb5557@psu.edu>
2019-11-17 16:08:42 -0500

    Finished parsing time data and correctly converting to minutes

--- Dataprocessing.py
+++ Dataprocessing.py
@@ -2,24 +2,31 @@ import pandas as pandas
 import math 
 
 label = []
+#mystring = []
+time = []
+time2 = []
+totaltime = []
 
 df =  pandas.read_csv('test.csv')
 df.dropna(axis = 1, how ='all', inplace = True)
 
 df.drop(['taxi_id','trip_end_timestamp','dropoff_census_tract','pickup_community_area','dropoff_community_area','tips','tolls','extras','trip_total','payment_type','company'], axis =1, inplace = True)
 df.dropna(inplace = True)
-print (df)
-list = df.loc[: , "fare"]
-#print (list)
-i = 0 
-for j in range(len(list)) :
-    result = list[j] * 0.2
-    label[j] = math.floor(result)
-    
+
+fare_list = df.loc[: , "fare"]
+datetime_list = df.loc[:,"trip_start_timestamp"]
+
+for fare in fare_list :
+    result = fare * 0.2
+    if (result > 20):
+        result = 20
+    label.append(math.floor(result))
+i = 0
+for row in datetime_list :
+    time.append(row.split(" ",1))
+    time2.append(time[i][1].split(":",))
+    totaltime.append(int(time2[i][0])*60 + int(time2[i][1]))
+    i += 1
+ 
 df['Fare_Label'] = label
-    
-   
-   
-        
-
-
+df['Time_Label'] = totaltime 
