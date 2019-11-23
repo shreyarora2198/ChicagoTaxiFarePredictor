ae48deb63fd83a982f780dd8aa76865797e49502
David Lengel <david@Davids-MBP.lan1>
2019-11-23 12:34:21 -0500

    Temp fix for extra column in ml.py

--- ml.py
+++ ml.py
@@ -9,6 +9,7 @@ Created on Sun Nov 17 12:30:01 2019
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
@@ -49,10 +50,12 @@ def nn_prediction():
     # Randomly split data into 70% training and 30% test
     X_train, X_test, y_train, y_test = train_test_split(datalist, targetlist, test_size=.30)
     
-    # create neural network clf model
+    learning_rate = 0.1
+    num_hidden_layers = 3
+
     clf = MLPClassifier(solver='sgd', activation='logistic', 
-        learning_rate_init=0.01, learning_rate='constant', max_iter=5000, verbose='true',
-        hidden_layer_sizes=(5,))
+     learning_rate_init=learning_rate, learning_rate='constant', max_iter=1000, verbose='true',
+     hidden_layer_sizes=(num_hidden_layers,))
     
     clf.fit(X_train, y_train)
 
@@ -60,15 +63,20 @@ def nn_prediction():
 
     return str(accuracy_score(y_test, result))
 
-
 df =  pd.read_csv('../filtered_data/01_January_filtered.csv')
 
 target = df.filter(['Fare_Label'], axis=1)
-df.drop(['Fare_Label'], axis = 1, inplace = True)
+
+# TEMPORARY! NEED TO REMOVE 'Unnamed: 0' from Dataprocessing.py
+df.drop(['Fare_Label', 'Unnamed: 0'], axis = 1, inplace = True)
 
 datalist = df.values.tolist()
-targetlist = target.values.tolist()
+temp_targetlist = target.values.tolist()
+
+targetlist = []
+for array in temp_targetlist:
+    targetlist.append(array[0])
 
 print("kNN accuracy: " + kNN_prediction())
-print("Naive Bayes accuracy: " + nb_prediction())
-print("Neural Network accuracy: ", nn_prediction())
+#print("Naive Bayes accuracy: " + nb_prediction())
+#print("Neural Network accuracy: ", nn_prediction())
