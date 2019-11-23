a381e64d38b87b6e0521ed5088351b5502b7e230
David Lengel <david@Davids-MBP.lan1>
2019-11-23 11:45:58 -0500

    Add kNN and NB models to ml

--- ml.py
+++ ml.py
@@ -8,13 +8,14 @@ Created on Sun Nov 17 12:30:01 2019
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 
 def kNN_prediction():
         
     # Randomly split data into 70% training and 30% test
-    X_train, X_test, y_train, y_test = train_test_split(datalist, targetlist, test_size=.20)
+    X_train, X_test, y_train, y_test = train_test_split(datalist, targetlist, test_size=.30)
     
     # Train a kNN model using the training set
     clf_KNN = KNeighborsClassifier()
@@ -26,6 +27,22 @@ def kNN_prediction():
     
     return str(accuracy_score(y_test, result))
 
+def nb_prediction():
+    
+    # Randomly split data into 70% training and 30% test
+    X_train, X_test, y_train, y_test = train_test_split(datalist, targetlist, test_size=.30)
+    
+    # Create NB classifier: 
+    nb = GaussianNB()
+
+    # Train the model
+    nb.fit(X_train, y_train)
+    
+    # Predictions
+    result = nb.predict(X_test)
+    
+    return str(accuracy_score(y_test, result));
+
 df =  pd.read_csv('../filtered_data/01_January_filtered.csv')
 
 target = df.filter(['Fare_Label'], axis=1)
@@ -34,4 +51,5 @@ df.drop(['Fare_Label'], axis = 1, inplace = True)
 datalist = df.values.tolist()
 targetlist = target.values.tolist()
 
-print("kNN accuracy: " + kNN_prediction())
+#print("kNN accuracy: " + kNN_prediction())
+print("Naive Bayes accuracy: " + nb_prediction())
